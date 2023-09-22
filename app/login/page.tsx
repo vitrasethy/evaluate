217dda71edfe33dd1d90ae30efe2c168a1036a79
vitrasethy <89233078+vitrasethy@users.noreply.github.com>
2023-09-22 19:03:14 +0700

    some edit

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
           </div>
         </div>
         <div className="w-full xl:w-1/3 2xl:w-3/12 md:w-8/12 md:h-[600px] min-xl:mr-[10%] min-[300px]:w-96 min-[300px]:h-[480px] max-[640px]:mx-[5%] max-[640px]:mb-16 bg-[#fff] drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)] pt-6 pb-14 px-10 rounded-2xl">
-          <h1 className="text-center min-[300px]:text-2xl md:text-4xl md:mt-2  font-medium mb-1 md:6">
+          <h1 className="text-center text-black min-[300px]:text-2xl md:text-4xl md:mt-2  font-medium mb-1 md:6">
             Login
           </h1>
           <Form />
